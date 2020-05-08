58f1b71f5538d0483273499dc057d0c2409c3659
mcrkvena <57915845+mcrkvena@users.noreply.github.com>
2020-05-08 17:48:22 +0200

    Add files via upload

--- src/array.js
+++ src/array.js
@@ -1,17 +1,17 @@
 let niz = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-function filtriranje(broj){
-    return broj>5
-}
-let filtriraniniz = niz.filter(filtriranje)
 
-function mapiranje(broj){
+let filtriraniniz = niz.filter(function(broj){
+    return broj >5
+})
+
+let mapiraniniz = niz.map(function(broj){
     return broj*3
-}
-let mapiraniniz = niz.map(mapiranje)
+})
 
 let reduciraniniz = niz.reduce(function(broj, suma){
     return broj + suma
 }, 0)
+
 console.log("filter: ", filtriraniniz)
 console.log("map: ", mapiraniniz)
 console.log("reduce: ", reduciraniniz)
